5660bef89fcbd21eaa51ac6ed9eb355af6b01635
A.Kolyadin <anton.geosys@gmail.com>
2018-02-17 23:45:48 +0300

    Мелкие правки

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,21 +1,22 @@
-var link = document.querySelector(".product-week__btn");
+var link = document.querySelectorAll(".to-cart__btn");
 var overlay = document.querySelector(".modal__overlay");
 var writeus = document.querySelector(".modal");
 var form = writeus.querySelector("form");
 var close = document.querySelector(".modal__overlay")
-
 var size = writeus.querySelector("[name=radio-size]");
-
 var storage_name = localStorage.getItem("name");
 
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  overlay.classList.add("modal__overlay--show");
-  writeus.classList.add("modal__show");
+for (var i = 0; i < link.length; i++)
+{
 
-  if(storage_name) {
-  name.value = storage_name;
-  }
+  link[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    overlay.classList.add("modal__overlay--show");
+    writeus.classList.add("modal__show");
+
+    if(storage_name) {
+    name.value = storage_name;
+    }
 });
 
 close.addEventListener("click", function(event) {
@@ -23,3 +24,4 @@ close.addEventListener("click", function(event) {
   writeus.classList.remove("modal__show");
   overlay.classList.remove("modal__overlay--show");
 });
+};
